8ee20cf78b66f183fc96c608a2b99de4af6e27c5
activemac <tachibana@active-c.jp>
2025-12-03 11:40:07 +0900

    Fixed url cache error

--- sw.js
+++ sw.js
@@ -1,23 +1,24 @@
-const VERSION = "v1.0.2";
+const VERSION = "v1.0.3";
 const CACHE_NAME = `period-tracker-${VERSION}`;
+const BASE_PATH = "/pwa_test";
 
 const APP_STATIC_RESOURCES = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/cycletracker.json",
-  "/icons/wheel.svg",
-  "/icons/web-app-manifest-192x192.png",
-  "/icons/web-app-manifest-512x512.png",
+  `${BASE_PATH}/`,
+  `${BASE_PATH}/index.html`,
+  `${BASE_PATH}/style.css`,
+  `${BASE_PATH}/app.js`,
+  `${BASE_PATH}/cycletracker.json`,
+  `${BASE_PATH}/icons/wheel.svg`,
+  `${BASE_PATH}/icons/web-app-manifest-192x192.png`,
+  `${BASE_PATH}/icons/web-app-manifest-512x512.png`,
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open(CACHE_NAME);
-      cache.addAll(APP_STATIC_RESOURCES);
-    })(),
+      await cache.addAll(APP_STATIC_RESOURCES);
+    })()
   );
 });
 
@@ -31,19 +32,19 @@ self.addEventListener("activate", (event) => {
             return caches.delete(cacheName);
           }
           return undefined;
-        }),
+        })
       );
       await self.clients.claim();
-    })(),
+    })()
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method === "navigate") {
+  if (event.request.mode === "navigate") {
     event.respondWith(
       (async () => {
         const cache = await caches.open(CACHE_NAME);
-        return cache.match("/") || cache.match("/index.html");
+        return cache.match(`${BASE_PATH}/`) || cache.match(`${BASE_PATH}/index.html`);
       })()
     );
     return;
@@ -52,11 +53,11 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async () => {
       const cache = await caches.open(CACHE_NAME);
-      const cachedResponse = await cache.match(event.request.url);
+      const cachedResponse = await cache.match(event.request);
       if (cachedResponse) {
         return cachedResponse;
       }
       return new Response(null, { status: 404 });
-    })(),
+    })()
   );
 });
